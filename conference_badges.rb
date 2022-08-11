cf16143de78ea6b74232b5dbcfc51c36db6c3669
Hillary Muller <hillaryaward@gmail.com>
2022-08-11 18:32:43 -0500

    commit tree 89c92ba61ae285a07a2f7500df8642c08a58b73e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(speakers)
+    room = 0
+    speakers.map do |speaker|
+        room += 1
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(names)
+    names.map do |name|
+        puts "Hello, my name is #{name}."
+    end
+    room = 0
+    names.map do |speaker|
+        room += 1
+        puts "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
